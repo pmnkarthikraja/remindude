ea9a9b4f50c162f0742e16dc2e2c60bc04a9e86d
karthikraja <pmnkarthikraja@gmail.com>
2024-08-08 12:52:04 +0530

    add serviceWorkerRegistration.ts file to trigger the service

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { register } from '../serviceWorkerRegistration';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
@@ -9,3 +11,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+register()
+
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,14 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    VitePWA({ registerType: 'autoUpdate' }),
     legacy({
       targets: ['defaults', 'not IE 11'] // Adjust this based on your browser support requirements
     })
