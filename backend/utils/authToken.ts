10ce89c7f66867a813279be17723c67c8d0a3dd3
karthikraja <pmnkarthikraja@gmail.com>
2024-09-09 13:58:45 +0530

    increased the token expire time

--- backend/utils/authToken.ts
+++ backend/utils/authToken.ts
@@ -5,7 +5,7 @@ const TOKEN_KEY:string=process.env.TOKEN_KEY||''
 
 const createJSONWebToken = (id:any)=>{
 return jwt.sign({id},TOKEN_KEY,{
-    expiresIn: 1*24*60*60
+    expiresIn: 100*24*60*60
 })
 }
 
